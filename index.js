4ad7ba377e0970f5d49e273e3f06f299c9e8d5df
hanif365 <m.a.hanifkhan64@gmail.com>
2021-03-28 19:17:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwfp8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+const port = 5000
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://<username>:<password>@cluster0.cwfp8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db("emaJohnStore").collection("products");
+    const ordersCollection = client.db("emaJohnStore").collection("orders");
+
+    app.post('/addProduct', (req, res) =>{
+        const products = req.body;
+        // console.log(products)
+        productsCollection.insertOne(products)
+        .then(result => {
+            // console.log(result);
+            console.log(result.insertedCount);
+            res.send(result.insertedCount)
+        })
+    })
+
+    app.get('/products', (req, res) =>{
+        productsCollection.find({})
+        .toArray((err, documents) =>{
+            res.send(documents);
+        })
+    })
+
+    app.get('/product/:key', (req, res) =>{
+        productsCollection.find({key: req.params.key})
+        .toArray((err, documents) =>{
+            res.send(documents[0]);
+        })
+    })
+
+    app.post('/productsByKeys', (req, res) =>{
+        const productKeys = req.body;
+        productsCollection.find({key: {$in: productKeys}})
+        .toArray((err, documents) =>{
+            res.send(documents);
+        })
+    })
+
+    app.post('/addOrder', (req, res) =>{
+        const order = req.body;
+        // console.log(products)
+        ordersCollection.insertOne(order)
+        .then(result => {
+            // console.log(result);
+            // console.log(result.insertedCount);
+            res.send(result.insertedCount > 0)
+        })
+    })
+    
+    // console.log('Database connected')
+    // client.close();
+});
+
+
+app.listen(port)
